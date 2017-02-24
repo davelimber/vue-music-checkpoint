2f98a7185d4e2bc9dc9b07b78c1d8304b0f2dfd5
dave limber <dave.limber@gmail.com>
2017-02-24 15:45:59 -0700

    moved to docs

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 
+import {store} from '.store/store'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   template: '<App/>',
   components: { App }
 })
